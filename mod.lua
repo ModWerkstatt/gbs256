ed767515addade690a3b5ea2cdad87e02833eaa8
Florian Thiel <flth@flth.de>
2024-08-25 14:15:48 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,20 @@
+function data()
+return {
+info = {
+  minorVersion = 0,
+  severityAdd = "NONE",
+  severityRemove = "WARNING",
+  name = _("mod_name"),
+  description = _("mod_desc"),
+  authors = {
+	{
+      name = "ModWerkstatt",
+      role = "CREATOR",
+    },
+  },
+  tags = { "europe", "germany", "bundesbahn", "goods", "waggon" },
+  		visible = true,
+},
+
+ }
+end
---
+++ strings.lua
@@ -0,0 +1,28 @@
+function data()
+return {
+		de = {
+			["mod_name"] = "Gbs 256 Spundwandwagen",
+			["mod_desc"] = "Die Waggonfabrik Uerdingen baut 1963/64 im Auftrag der DB nach einem Prototyp eine Serie von 300 zweiachsigen gedeckten Güterwagen, die anfangs die Bezeichnung Glmms 64 erhalten, jedoch noch 1964 in Gbs 256 umgezeichnet werden. Als einzige DB-Bauart werden sie in Ganzstahl-Leichtbauweise gefertigt und sind für den Einbau einer automatischen Kupplung vorbereitet. Der Wagenboden besteht aus Kiefernbohlen, der Innenraum ist mit einer 3 mm dicken Korkschrotschicht isoliert.",
+			
+            ["Gbs256_menu"] = "Gbs 256",
+			["gbs256_1_name"] = "Gbs 256 DB",
+			["gbs256_1_desc"] = "Die ursprüngliche Bezeichnung Glmms 64 wurde im Jahr 1964 an die UIC-Richtlinien angepaßt und in Gbs 256 umgezeichnet. Die Verwendung der Wagen änderte sich dadurch nicht und sie waren als einzige Ganzstahlwagen bei der Bundesbahn unterwegs.",
+		
+			["gbs256_2_name"] = "Gbs 256 DB AG",
+			["gbs256_2_desc"] = "Mit Gründung der Deutschen Bahn AG im Jahr 1994 wurden noch 220 Wagen übernommen. Da die Wagen aber am Ende ihrer Lebenszeit angekommen waren, wurde alle Exemplare bis zum Jahr 1999 ausgemustert.",
+			
+		},
+		en = {
+			["mod_name"] = "Gbs 256 Sheet pile wagon",
+			["mod_desc"] = "In 1963/64, the Uerdingen wagon factory built a series of 300 two-axle boxcars based on a prototype on behalf of the DB. They are the only DB type that are made of all-steel lightweight construction and are prepared for the installation of an automatic coupling. The floor of the car is made of pine planks, the interior is insulated with a 3 mm thick layer of cork pellets.",
+
+            ["Gbs256_menu"] = "Gbs 256",
+			["gbs256_1_name"] = "Gbs 256 Bundesbahn",
+			["gbs256_1_desc"] = "The original designation Glmms 64 was adapted to the UIC guidelines in 1964 and changed to Gbs 256. The use of the wagons did not change and they were the only all-steel wagons on the Federal Railroad.",
+			
+			["gbs256_2_name"] = "Gbs 256 DB AG",
+			["gbs256_2_desc"] = "When Deutsche Bahn AG was founded in 1994, 220 cars were taken over. However, since the cars had reached the end of their life, all waggons were taken out of service by 1999.",
+			
+		},
+	}
+end
