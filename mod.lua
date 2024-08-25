3f8e2bea2b67f071f8234bbc18ccbb8d61ca5f57
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-08-25 14:43:41 +0200

    Update auf 1.1
    Umbau auf Basis Mod
    - Sound neu
    - Zg2 neu

--- mod.lua
+++ mod.lua
@@ -1,20 +1,44 @@
 function data()
-return {
-info = {
-  minorVersion = 0,
-  severityAdd = "NONE",
-  severityRemove = "WARNING",
-  name = _("mod_name"),
-  description = _("mod_desc"),
-  authors = {
-	{
-      name = "ModWerkstatt",
-      role = "CREATOR",
-    },
-  },
-  tags = { "europe", "germany", "bundesbahn", "goods", "waggon" },
-  		visible = true,
-},
+  return {  
+	info = {
+		minorVersion = 1,
+		severityAdd = "NONE",
+		severityRemove = "WARNING", 
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },  
+		},
+		params = {			
+			{
+				key = "soundCheck",
+				name = _("sound_check"),
+				uiType = "CHECKBOX",
+				values = { "No", "Yes", },				
+				defaultIndex = 1,	
+			},
+		},
+		tags = { "Europe", "Vehicle", "Freight", "Wagon", "Gbs", "Transwaggon", "DB", },
+		dependencies = {},
+		}, 
+		
+		runFn = function (settings, modParams)
+		local params = modParams[getCurrentModId()]
+	
+		local function metadataHandler(fileName, data)
+			if params.soundCheck == 0 then
+				if fileName:match('/vehicle/waggon/gbs256/gbs256_([^/]*.mdl)')
+				then
+					data.metadata.railVehicle.soundSet.name = "waggon_freight_modern"
+				end
+			end
+			return data
+		end
 
- }
+		addModifier( "loadModel", metadataHandler )
+	end,
+}
 end
--- strings.lua
+++ strings.lua
@@ -3,6 +3,9 @@ return {
 		de = {
 			["mod_name"] = "Gbs 256 Spundwandwagen",
 			["mod_desc"] = "Die Waggonfabrik Uerdingen baut 1963/64 im Auftrag der DB nach einem Prototyp eine Serie von 300 zweiachsigen gedeckten Güterwagen, die anfangs die Bezeichnung Glmms 64 erhalten, jedoch noch 1964 in Gbs 256 umgezeichnet werden. Als einzige DB-Bauart werden sie in Ganzstahl-Leichtbauweise gefertigt und sind für den Einbau einer automatischen Kupplung vorbereitet. Der Wagenboden besteht aus Kiefernbohlen, der Innenraum ist mit einer 3 mm dicken Korkschrotschicht isoliert.",
+				
+			["sound_check"] = "Eigenes Soundset aktivieren?",
+			["option_sound_check"] = "Soll der extra Sound aktiviert sein?",
 			
             ["Gbs256_menu"] = "Gbs 256",
 			["gbs256_1_name"] = "Gbs 256 DB",
@@ -15,7 +18,10 @@ return {
 		en = {
 			["mod_name"] = "Gbs 256 Sheet pile wagon",
 			["mod_desc"] = "In 1963/64, the Uerdingen wagon factory built a series of 300 two-axle boxcars based on a prototype on behalf of the DB. They are the only DB type that are made of all-steel lightweight construction and are prepared for the installation of an automatic coupling. The floor of the car is made of pine planks, the interior is insulated with a 3 mm thick layer of cork pellets.",
-
+			
+			["sound_check"] = "Activate custom sound?",
+			["option_sound_check"] = "Should the extra sound be activated?",
+			
             ["Gbs256_menu"] = "Gbs 256",
 			["gbs256_1_name"] = "Gbs 256 Bundesbahn",
 			["gbs256_1_desc"] = "The original designation Glmms 64 was adapted to the UIC guidelines in 1964 and changed to Gbs 256. The use of the wagons did not change and they were the only all-steel wagons on the Federal Railroad.",
